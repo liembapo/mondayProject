82c41de64f3d2a5531f89fd46a91ae9dcc940fd1
Aruuke <akydyralieva06@gmail.com>
2019-11-20 17:43:40 -0600

    this is my message

--- src/git/Hub.java
+++ src/git/Hub.java
@@ -3,10 +3,19 @@ package git;
 public class Hub {
     public static void main(String[] args) {
         //PRINT 5 TIMES YOUR NAME;
+
         System.out.println("Anton Semenov");
         System.out.println("Anton Semenov");
         System.out.println("Anton Semenov");
         System.out.println("Anton Semenov");
         System.out.println("Anton Semenov");
+
+        System.out.println("Aruukee");
+        System.out.println("Aruuke");
+        System.out.println("Aruuke");
+        System.out.println("Aruuke");
+        System.out.println("Aruuke");
+
+
     }
 }
